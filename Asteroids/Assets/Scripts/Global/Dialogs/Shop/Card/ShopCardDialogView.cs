c2bd03275dce4e921250fa64aa1bc26a011ef7bb
av6eev <av6eev@gmail.com>
2023-08-22 14:50:16 +0300

    add: statistic of ship to shop card prefab

--- Asteroids/Assets/Scripts/Global/Dialogs/Shop/Card/ShopCardDialogView.cs
+++ Asteroids/Assets/Scripts/Global/Dialogs/Shop/Card/ShopCardDialogView.cs
@@ -1,4 +1,5 @@
-﻿using Global.Dialogs.Base;
+﻿using System.Globalization;
+using Global.Dialogs.Base;
 using Specifications.Ships;
 using TMPro;
 using UnityEngine;
@@ -8,13 +9,20 @@ namespace Global.Dialogs.Shop.Card
 {
     public class ShopCardDialogView : BaseDialogView
     {
+        [field: Header("Ship Preview")]
         [field: SerializeField] public Image PreviewImage { get; private set; }
         [field: SerializeField] public TextMeshProUGUI TitleText { get; private set; }
         [field: SerializeField] public TextMeshProUGUI PriceText { get; private set; }
+        [field: SerializeField] public TextMeshProUGUI GainedText { get; private set; }
         [field: SerializeField] public Button NextCardButton { get; private set; }
         [field: SerializeField] public Button PreviousCardButton { get; private set; }
         [field: SerializeField] public Button BuyButton { get; private set; }
         [field: SerializeField] public Button SelectButton { get; private set; }
+        [field: Header("Ship Stats")]
+        [field: SerializeField] public TextMeshProUGUI HealthStatText { get; private set; }
+        [field: SerializeField] public TextMeshProUGUI SpeedStatText { get; private set; }
+        [field: SerializeField] public TextMeshProUGUI AutomaticStatText { get; private set; }
+        [field: SerializeField] public TextMeshProUGUI ClipStatText { get; private set; }
 
         public void SetupCard(ShipSpecification specification)
         {
@@ -29,6 +37,10 @@ namespace Global.Dialogs.Shop.Card
 
             TitleText.text = specification.Name;
             PriceText.text = specification.Price.ToString();
+            HealthStatText.text = specification.Health.ToString();
+            SpeedStatText.text = specification.Speed.ToString(CultureInfo.InvariantCulture);
+            AutomaticStatText.text = specification.IsAutomatic ? "Да" : "Нет";
+            ClipStatText.text = specification.Count.ToString();
         }
 
         public void SwitchButtons(bool isPurchased)
@@ -41,12 +53,8 @@ namespace Global.Dialogs.Shop.Card
         {
             if (!isPurchased) return;
             
-            var textColor = PriceText.color;
-            textColor.a = .5f;
-            
-            PriceText.color = textColor;
-            PriceText.text = "Получено";
-            PriceText.GetComponentInChildren<Image>().enabled = false;
+            PriceText.gameObject.SetActive(false);
+            GainedText.gameObject.SetActive(true);
         }
     }
 }
